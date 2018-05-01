0e9c32d07ec8deb75434ad375e1a10e9dc01fd19
Matthew Schwartz <schwartz_matthew2@bah.com>
2018-04-30 21:55:46 -0400

    initial commit

---
+++ data.js
@@ -0,0 +1,112 @@
+var data = [{
+    "A": "Act 77",
+    "B": "Act 77, Flexible Pathways Initiative became law in July of 2013 and represents the commitment of Vermont citizens, educators, and lawmakers to establish a policy-friendly environment that fosters a system of public education in which every student graduates and every high school graduate is college and career ready. At the heart of Act 77 is that all students develop a Personalized Learning Plan (PLP) which identifies their own personal roadmap to graduation.",
+    "C": [{
+      "text": "Flexible Pathways to Graduation: Six Vermont High School Students (Video)",
+      "link": "https://vimeo.com/197344463"
+    }, {
+      "text": "Student Centered Learning",
+      "link": "https://www.youtube.com/watch?time_continue=19&v=e6ieXLVCss4"
+    }, {
+      "text": "Flexible Pathways in Vermont: The Route Changes, but the Destination Stays the Same",
+      "link": "https://www.youtube.com/watch?time_continue=52&v=8iYx7X8uqbI"
+    }, {
+      "text": "Introduction to Act 77",
+      "link": "http://education.vermont.gov/sites/aoe/files/documents/edu-introduction-to-act-77.pdf"
+    }]
+  },
+  {
+    "A": "Personalized Learning Plans",
+    "B": "Personalized Learning describes systems and approaches that deepen student learning by incorporating each student’s interests, strengths and needs - including student voice and choice in what, how, when and where they learn - to achieve the goals of active engagement, academic success, and preparation for post-secondary opportunities. Personalized learning and personalized instructional approaches are critical to students in kindergarten through grade 6 as well as grades 7-12.",
+    "C": [{
+      "text": "PLP Framework for Adults",
+      "link": "http://education.vermont.gov/sites/aoe/files/documents/edu-plp-conceptual-framework-for-adults.pdf"
+    }, {
+      "text": "PLP Framework for Students",
+      "link": "http://education.vermont.gov/sites/aoe/files/documents/edu-plp-conceptual-framework-for-students.pdf"
+    }, {
+      "text": "Critical Elements of a PLP",
+      "link": "http://education.vermont.gov/sites/aoe/files/documents/edu-plp-critical-elements.pdf"
+    }, {
+      "text": "PLP Example",
+      "link": "https://sites.google.com/a/wwsu.org/katie-k-plp/"
+    }, {
+      "text": "PLP Sample(Video)",
+      "link": "https://spark.adobe.com/video/C1MRw5Surg3Hb"
+    }]
+  },
+  {
+    "A": "Work-Based Learning",
+    "B": "Experiences in which students can interact with industry or community professionals in real, virtual, or simulated work environments that expose learners to postsecondary options, provide opportunities for skill development, and allow students to reinforce their school-based learning.",
+    "C": [{
+      "text": "Work Based Learning Manual",
+      "link": "http://education.vermont.gov/sites/aoe/files/documents/edu-work-based-learning-manual.pdf"
+    }]
+  },
+  {
+    "A": "Career and Technical Education",
+    "B": "Designed to provide students with technical knowledge and skills that will prepare them for graduation, further education and enhance their employment options or may lead to an industry-recognized credential. CTE is available to Vermont high school students through 17 regional CTE centers.",
+    "C": [{
+      "text": "What is Career Technical Education?",
+      "link": "http://education.vermont.gov/student-learning/flexible-pathways/career-technical-education"
+    }]
+  },
+  {
+    "A": "Dual Enrollment",
+    "B": "Enrollment by a high school student in a credit-bearing course offered by a college/university. Upon successful completion of the course, the student will receive credit toward graduation from their high school and college credit from the college/university.",
+    "C": [{
+        "text": "Eligibility and Requirements",
+        "link": "http://education.vermont.gov/student-learning/flexible-pathways/dual-enrollment"
+      },
+      {
+        "text": "Dual Enrollment Program Manual",
+        "link": "http://education.vermont.gov/sites/aoe/files/documents/edu-dual-enrollment-program-manual-2017-1.pdf"
+      },
+      {
+        "text": "Dual Enrollment Voucher Request",
+        "link": "https://andromeda.ccv.vsc.edu/DualEnrollment/StudentVT/EligibilityInfo.cfm"
+      }
+    ]
+  }, {
+    "A": "Virtual/\nBlended Learning",
+    "B": "Learning delivered through a web-based platform that employs a variety of digital tools, content, and supports and allows for student choice around time, place, path, and/ or pace. Virtual learning can facilitate both individual and collective learning, and can occur synchronously and asynchronously.",
+    "C": []
+  }, {
+    "A": "Early College",
+    "B": "Full-time enrollment by a 12th grade Vermont student for one academic year in a program offered by a partnering college/university. Upon successful completion, students receive a diploma from their high school and college credit fro",
+    "C": [{
+      "text": "Getting to Early College",
+      "link": "http://education.vermont.gov/student-learning/flexible-pathways/early-college"
+    }]
+  }, {
+    "A": "Community Based Learning",
+    "B": "A program that combines meaningful service to the community with a student’s learning goals. Students apply what they learn in school to a valuable, significant, and necessary service in their communities.",
+    "C": [{
+      "text": "What’s Going On with Community Based Learning?",
+      "link": "http://tiie.w3.uvm.edu/blog/community-based-learning-in-vermont/#.WtZuUy7wbDd"
+    }, {
+      "text": "Student Views about Community Based Learning",
+      "link": "http://www.montpelierbridge.com/2016/04/community-based-learning-a-view-of-a-students-involvement-with-corporations/"
+    }, {
+      "text": "Community Based Learning (video)",
+      "link": "https://spark.adobe.com/video/Bkdpzj27"
+    }]
+  }, {
+    "A": "Expanded Learning",
+    "B": "High quality programs within communities and schools designed to serve learners on a regular basis by providing unique opportunities for academic growth, hands-on learning, and personal development. These opportunities can occur beyond the traditional school hours and outside the school building.",
+    "C": [{
+      "text": "Expanded Learning Example",
+      "link": "https://www.vtlff.org/expanded-learning-opportunity-network/"
+    }]
+  }, {
+    "A": "High School Completion",
+    "B": "Designed to offer a flexible pathway to those at least 16 years of age and at risk of disengaging from school, the HSCP provides learners with educational services of the scope and rigor needed for the attainment of a high school diploma. HSCP is managed by local Adult Education and Literacy providers in connection with a student’s assigned high school.",
+    "C": [{
+      "text": "Completion Program",
+      "link": "http://www.vtadultlearning.org/services/high-school-completion/"
+    }, {
+      "text": "High School Completion Program Manual",
+      "link": "http://education.vermont.gov/sites/aoe/files/documents/edu-adult-education-hscp-policy-manual-fy18_0.pdf"
+    }]
+  }
+];
